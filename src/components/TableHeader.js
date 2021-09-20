c588ee5bf75c68587d01f8aee2d45673648bdbc0
Michael Williams <mwilliams2964@gmail.com>
2021-09-20 10:04:28 -0700

    cleaned up sorting logic

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -3,57 +3,58 @@ import React, { useState } from "react";
 function TableHeader(props) {
   const [descending, setDescending] = useState(false);
 
-  const sortByHeader = (header) => {
-    const orderClone = [...props.order];
-    if (descending) {
-      orderClone.sort((a, b) => {
-        const getAccessorFromType = (header) => {
-          if (typeof header === "string") {
-            return header.toLowerCase();
-          } else if (typeof header === "number") {
-            return header;
-          } else if (Array.isArray(header)) {
-            return header[0];
-          }
-        };
-        if (getAccessorFromType(a[header]) < getAccessorFromType(b[header])) {
-          return 1;
-        } else if (
-          getAccessorFromType(a[header]) > getAccessorFromType(b[header])
-        ) {
-          return -1;
-        }
-        return 0;
-      });
-    } else {
-      orderClone.sort((a, b) => {
-        const getAccessorFromType = (header) => {
-          if (typeof header === "string") {
-            return header.toLowerCase();
-          } else if (typeof header === "number") {
-            return header;
-          } else if (Array.isArray(header)) {
-            return header[0];
-          }
-        };
-        if (getAccessorFromType(a[header]) < getAccessorFromType(b[header])) {
-          return -1;
-        } else if (
-          getAccessorFromType(a[header]) > getAccessorFromType(b[header])
-        ) {
-          return 1;
-        }
-        return 0;
-      });
+  const getAccessorFromType = (header) => {
+    if (typeof header === "string") {
+      return header.toLowerCase();
+    } else if (typeof header === "number") {
+      return header;
+    } else if (Array.isArray(header)) {
+      return header[0];
     }
+  };
 
+  const sortAscending = (header) => {
+    const orderClone = [...props.order];
+    orderClone.sort((a, b) => {
+      if (getAccessorFromType(a[header]) < getAccessorFromType(b[header])) {
+        return 1;
+      } else if (
+        getAccessorFromType(a[header]) > getAccessorFromType(b[header])
+      ) {
+        return -1;
+      }
+      return 0;
+    });
     props.setOrder(orderClone);
+  };
+
+  const sortDescending = (header) => {
+    const orderClone = [...props.order];
+    orderClone.sort((a, b) => {
+      if (getAccessorFromType(a[header]) < getAccessorFromType(b[header])) {
+        return -1;
+      } else if (
+        getAccessorFromType(a[header]) > getAccessorFromType(b[header])
+      ) {
+        return 1;
+      }
+      return 0;
+    });
+    props.setOrder(orderClone);
+  };
+
+  const toggleSortDirection = (header) => {
+    if (descending) {
+      sortAscending(header);
+    } else {
+      sortDescending(header);
+    }
   };
 
   return (
     <th
       onClick={() => {
-        sortByHeader(props.header);
+        toggleSortDirection(props.header);
         setDescending(!descending);
       }}
     >
